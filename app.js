ad7888d800a42aebf99847ceec42ca32d776b9e2
Aggrim Arora <aggrimarora@Aggrims-MacBook-Pro.local>
2019-05-21 13:57:47 -0400

    hashing passwords

--- app.js
+++ app.js
@@ -5,24 +5,26 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const mongo = require('mongo');
-const encrypt = require('mongoose-encryption');
 const app = express();
+const md5 = require('md5');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+//create a new database userDB
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
+//schema for userDB documents
 const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
+//get requests ofr different routes---------------------------------------------
 app.get("/", function(req, res) {
   res.render("home");
 });
@@ -35,10 +37,11 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+//post requests ofr different routes---------------------------------------------
 app.post("/register", function(req, res) {
   var user = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   user.save(function(err) {
     if(err) {
@@ -56,13 +59,14 @@ app.post("/login", function(req, res) {
       console.log(err);
     }
     else {
-      if(req.body.password === foundUser.password) {
+      if(md5(req.body.password) === foundUser.password) {
         res.render("secrets");
       }
     }
   });
 });
 
+//Server
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
 })
